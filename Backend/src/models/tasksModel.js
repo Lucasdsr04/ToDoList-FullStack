b75e69455ce50475885cad2e96493ced54139b1e
Lucas dos Santos <sl.solucas@gmail.com>
2023-03-28 21:07:03 -0300

    Configurações iniciais do projeto
    Configurações iniciais do projeto

---
+++ Backend/src/models/tasksModel.js
@@ -0,0 +1,9 @@
+const connection = require('./connection');
+
+const getAll = () => {
+
+};
+
+module.exports = {
+    getAll
+};
---
+++ Backend/src/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+// router.get('/tasks', (req, res) => response.status(200).send('Olá, Mundo'));
+
+
+
+module.exports = router;
